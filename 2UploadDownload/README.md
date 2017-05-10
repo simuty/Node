bbdf0f5c949002474a7936a381e011aaa22d1968
BWF-HHW <51Code@BWF-huanghaowei.local>
2017-05-10 14:11:12 +0800

    上传文件夹 path fs 模块 “

--- 2UploadDownload/README.md
+++ 2UploadDownload/README.md
@@ -3,8 +3,10 @@
 
 主要功能:
 
-1. 采用**formidable**进行表单的上传下载;
-2. 参数相关内容
+1. 采用**formidable**进行表单的上传下载、参数相关内容;
+2. 上传单个文件;
+3. 上传**文件夹**
+
 
 **form的属性及方法**
 
@@ -17,7 +19,7 @@
 6. multiples 一次上传多个文件 默认为false 需要在input标签设置HTML5属性 multiple
 hash
 7. bytesReceived 目前接收到的字节数。
-8. bytesExpected 预定总大小 字节
+8. bytesExpected 预定总大小字节
 ```
 ***parse(req,[cb])**
 
@@ -27,11 +29,11 @@ hash
 err 错误
 field 对象 是http请求Format的键值对即application/x-www-urlencoded内容
 files 接收到的文件对象（当接受多个文件时为一个数组）
-files.size 文件大小 单位是字节
-files.path 缓存的目录及名字
-files.name 上传时候的文件名
-files.type 文件后缀类型 image/gif之类
-files.mtime 最后修改时间
+files.x.size 文件大小 单位是字节
+files.x.path 缓存的目录及名字
+files.x.name 上传时候的文件名
+files.x.type 文件后缀类型 image/gif之类
+files.x.mtime 最后修改时间
 
 ```
 
@@ -43,9 +45,11 @@ files.mtime 最后修改时间
 ```
 node server.js
 ```
-在浏览器中访问： http://127.0.0.1:3000/
+**上传单个文件：**
+在浏览器中访问： http://127.0.0.1:4000/ 上传单文件内容，具体实现在**upload**路由中；页面在index.html中
 
-
+**上传文件夹**
+在浏览器中访问： http://127.0.0.1:4000/ 上传单文件内容，具体实现在**uploads**路由中；页面在indexz.html中
 
 配置详看: package.json;
 
@@ -53,5 +57,3 @@ node server.js
 
 
 [更多详见](http://www.jianshu.com/notebooks/6725301/latest)
-
-
--- 2UploadDownload/server.js
+++ 2UploadDownload/server.js
@@ -2,9 +2,12 @@ var express = require('express');
 var formidable = require('formidable');
 var fs = require('fs');
 var util = require('util');
+var path = require('path');
 
 var app = express();
+app.listen(4000);
 
+//---------------单个文件上传---------------------//
 app.get('/', function(req, res, next){
       fs.readFile('./index.html', function(err, data) {
       res.writeHead(200, {'Content-Type': 'text/html'});
@@ -29,9 +32,52 @@ app.post('/upload', function(req, res, next){
      //上传文件的名称
      var filename = files.upload.name;
      var path = files.upload.path;
-     //更改名称
+     //移动并更名
      fs.rename(path, form.uploadDir + filename);
      //响应 格式化打印
+     res.end(util.inspect(files));
+   });
+});
+
+//---------------文件夹上传---------------------//
+app.get('/files', function(req, res, next){
+      fs.readFile('./indexz.html', function(err, data) {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        res.end();
+      });
+})
+
+app.post('/uploads', function(req, res, next){
+  //form表单
+   var form = new formidable.IncomingForm();
+   //保留后缀
+   form.keepExtensions = true;
+   form.encoding = 'utf-8';
+   //上传文件路径,采用path路径拼接
+   form.uploadDir = path.join(__dirname, 'Files');
+   //如果上传文件夹（多个文件）需将 默认值改为TRUE
+  form.multiples = true;
+  //该方法会转换请求中所包含的表单数据，callback会包含所有字段域和文件信息
+  // fields 是普通表单数据
+  // files 是文件数据
+  form.parse(req, function(err, fields, files) {
+     res.writeHead(200, {'Content-type': 'text/plain'});
+     //该属性upload是在HTML文件的name中设置的
+     for (let file of files.upload) {
+       let pathnameArray = file.name.split('/');
+       let folderPath = path.join(form.uploadDir, pathnameArray[0]);
+       //如果不存在上传文件夹名称，就创建
+       if (!fs.existsSync(folderPath)) {
+          fs.mkdir(folderPath, 0777, function (err) {
+              if (err) throw err;
+          });
+       }
+       //移动文件夹并更改名称
+      //  fs.rename(oldpath, newpath, callback)
+      fs.rename(file.path, path.join(folderPath, pathnameArray[1]));
+     }
+     //响应 格式化打印 String
      res.end(util.inspect(files));
    });
 });
@@ -45,6 +91,3 @@ app.get('/download', function(req, res, next){
       console.log(err);
     })
 })
-
-
-app.listen(3000);
